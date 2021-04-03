4ed18ac4bb2e99cc25ffb56c2ec8bb636c17f257
Liton Kumar Biswas <litonkb97@gmail.com>
2021-04-04 00:35:30 +0600

    Updated UI

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'pages/index.html')
+
+def jobs(request):
+    return render(request,'jobs/jobs.html')
+
+def job_details(request):
+    return render(request,'jobs/job_details.html')
+def register(request):
+    return render(request,'accounts/register.html')
+
+def login(request):
+    return render(request,'accounts/login.html')
