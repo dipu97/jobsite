f768603591573008653c3140ee567eea223f13d6
Liton Kumar Biswas <litonkb97@gmail.com>
2021-04-04 10:08:33 +0600

    Ui Changes

--- jobsite/settings.py
+++ jobsite/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'jobsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+#
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
