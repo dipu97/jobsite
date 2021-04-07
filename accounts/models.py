c19546bc0bb168cf2240fff33a5f5bac494db1c9
shojibhasan <shojibhasan15@gmail.com>
2021-04-07 14:06:47 +0600

    registration error fixed

--- accounts/models.py
+++ accounts/models.py
@@ -15,11 +15,11 @@ class JobSeekers(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     address = models.CharField(max_length=250,blank=True,null=True)
     nid = models.CharField(max_length=20,blank=True,null=True)
-    image = models.ImageField(null=True,blank=True)
+    image = models.ImageField(upload_to='photos/%y/%m/%d',blank=True)
     area_of_interest = models.CharField(null=True,blank=True,max_length=250)
     resume = models.FileField(upload_to='resume/%y/%m/%d',blank=True)
     phone = models.IntegerField(blank=True,null=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)
 class Employeers(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     company_name = models.CharField(max_length=30,null=True,blank=True)
@@ -28,3 +28,4 @@ class Employeers(models.Model):
     company_type = models.CharField(max_length=250,blank=True,null=True)
     website_url = models.CharField(max_length=250,blank=True,null=True)
     contact = models.IntegerField(max_length=12,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
